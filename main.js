98200caf03293fb08a89482482ff6babd9568378
sauce-picante <tdougherty16@gmail.com>
2022-09-13 17:51:04 -0400

    made successful api call

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 
-//const map = L.map('map').setView([39.952325, -75.163705], 10);
+const map = L.map('map').setView([39.952325, -75.163705], 10);
 
 //map tiles
-//L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//maxZoom: 19,
-//attribution: '© OpenStreetMap'
-//}).addTo(map);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+maxZoom: 19,
+attribution: '© OpenStreetMap'
+}).addTo(map);
 
 
 
@@ -13,31 +13,30 @@
 /**
  *  Goal: Retrieve ongitude and latitude of a SEPTA regional train
  *  and add a marker to the map
- *  >   1. figure out successful api call
- *           a. ISSUE: call is correct but not going through
- *              i. TRY: process the API call with AJAX
- *  >   2. Retrieve data
+ *  >   1. figure out successful api call - DONE
+ *  >   2. Retrieve data - DONE
  *  >   3. Store data
+ *  >   4. Display Data
  *  >  
  */
 
 $(document).ready(function(){
     
-      $.ajax({
-        type: 'GET',
-        url: "https://www3.septa.org/api/TrainView/index.php",
-        contentType: "application/json",
-        dataType: 'json',
-        sucess: function(result){
-          console.log('SUCCESS');
-          $.each(result, function(i, item){
-            let trainNumber = item.trainno;
-            if(item.line == "West Trenton")
-            {
-                alert('Train Number: ' + trainNumber);
-            }
-          });
-        }
+  $.ajax({
+    url: "https://www3.septa.org/api/TrainView/index.php?&callback=?",
+    type: 'GET',
+   dataType: "jsonp",
+    success: function(data){
+      $.each(data, function(i,item){
+          var trainNumber =item.trainno;
+          if( item.line== 'West Trenton')
+          {
+              alert("Train Number " + trainNumber + ' ' + item.dest);
+          }
+  
       });
+    }
+  });
+  
   
 });
