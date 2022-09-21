016e4a7a05da870614c58cee54abccb191962d0f
sauce-picante <tdougherty16@gmail.com>
2022-09-21 12:19:03 -0400

    fixed map icon flickering

--- display_helper.js
+++ display_helper.js
@@ -5,18 +5,16 @@
 * >     Trolleys - IN PROGRESS
 *
 */
-function displayTrainCurrentLoc(item) {
+function displayTrainCurrentLoc(item,icon, trainLayer) {
     let trainNumber = item.trainno;
-    let trainIcon = L.icon({
-        iconUrl: './packages/leaflet/images/SEPTA_train.png',
-        iconSize: [27, 27 ]
-    });
     let trainMarker = L.marker([item.lat, item.lon], {
-        icon: trainIcon
+        icon: icon
     }).addTo(trainLayer);
+    trainLayer.addTo(map);
     trainMarker.bindPopup(`<b><h3>Train No. </b> ${trainNumber}<br>` + `<b>Next Stop: </b> ${item.nextstop} <br>` 
                           + `<b>Line: </b> ${item.line}<br>` + `<b> Destination: </b> ${item.dest}</h3>`);
-  }
+
+}
   
   function displayStationLoc(item) {
     let stationMarker = L.circleMarker([item.location_lat, item.location_lon], {
@@ -30,16 +28,18 @@ function displayTrainCurrentLoc(item) {
   }
   
   
-  function displayTrolleyLoc(item,route) {
+  function displayTrolleyLoc(item) {
+
+    console.log(item[0].destination);
     let trolleyIcon = L.icon({
         iconUrl: './packages/leaflet/images/trolley1.png',
         iconSize: [20, 20]
     });
-    let trolleyMarker = L.marker([item[route].lat, item[route].lng], {
+    let trolleyMarker = L.marker([item[0].lat, item[0].lng], {
         icon: trolleyIcon
     }).addTo(trolleyLayer);
-    trolleyMarker.bindPopup(`<b><h> Route ${item[route]}</b><br>` + `Vehicle: ${item[route].vehicleID}<br>` +
-        `Next Stop: ${item[route].next_stop_name}<br>` + `Destination: ${item[route].destination}</h>`);
+    trolleyMarker.bindPopup(`<b><h> Route ${item[0].route}</b><br>` + `Vehicle: ${item[0].vehicleID}<br>` +
+       `Next Stop: ${item[0].next_stop_name}<br>` + `Destination: ${item[0].destination}</h>`);
   }
   
   
