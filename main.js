a62137f9662a6dfd5fd24d46652b1358f3ddabd0
sauce-picante <tdougherty16@gmail.com>
2022-09-24 09:01:43 -0400

    moved api calls into a separate file for readability

--- main.js
+++ main.js
@@ -20,50 +20,13 @@ $(document).ready(function() {
       "https://www3.septa.org/api/locations/get_locations.php?lon=-75.221&lat=40.241&type=rail_stations&radius=55&callback=?",
       "https://www3.septa.org/api/locations/get_locations.php?lon=-75.094&lat=40.184&type=rail_stations&radius=55&callback=?"
   ];
-
-  let trolleyStopURLs = ["https://www3.septa.org/api/Stops/index.php?req1=10",
-      "https://www3.septa.org/api/Stops/index.php?req1=11",
-      "https://www3.septa.org/api/Stops/index.php?req1=15",
-      "https://www3.septa.org/api/Stops/index.php?req1=34",
-      "https://www3.septa.org/api/Stops/index.php?req1=36",
-      "https://www3.septa.org/api/Stops/index.php?req1=101",
-      "https://www3.septa.org/api/Stops/index.php?req1=102"
-  ];
   
 
   $("#trainInfo").on('click', function(event) {
     map.flyTo([39.952325, -75.163705],10);
-    sendRequest();
-    function sendRequest(){
-        $.ajax({
-            url: "https://www3.septa.org/api/TrainView/index.php?&callback=?",
-            type: 'GET',
-            dataType: "jsonp",
-            success: function(data) {
-                trainLayer.clearLayers();
-                $.each(data, function(i, item) {
-                displayTrainCurrentLoc(item, trainIcon, trainLayer);
-                });
-              },
-              complete: function(){
-                setInterval(sendRequest, 5000);
-              }
-          });
-    }
-    setTimeout( function(){
-    $.each(railStationURLs, function(i, u) {
-          $.ajax(u, {
-              type: 'GET',
-              dataType: 'jsonp',
-              success: function(data) {
-                  $.each(data, function(i, item) {
-                      displayStationLoc(item);
-                  });
-              }
-          });
-      });
-    }, 500);
-      event.preventDefault();
+    setInterval(getRegionalRailLoc(), 7000);
+    getRegionalRailStations(railStationURLs);
+    event.preventDefault();
   }); //End Train Button Event Handler
 
   $("#trolleyInfo").on('click', function(event) {
@@ -71,37 +34,10 @@ $(document).ready(function() {
     if(route == "" || route == null || route == undefined){
         alert("Please select a route!");
     } else {
-            console.log(route);
-            sendRequest(route);
-            function sendRequest(route){
-                let trolleyUrl = `https://www3.septa.org/api/TransitView/index.php?route=${route}`;
-                console.log(trolleyUrl);
-                trolleyLayer.clearLayers();
-            $.ajax({
-                    url: trolleyUrl,
-                    type: 'GET',
-                    dataType: 'jsonp',
-                    success: function(data){
-                        $.each(data, function(i,item,route) {
-                            
-                            displayTrolleyLoc(item,route);
-                        });
-                    }
-            });
-            }
-        }
-    /**$.each(trolleyStopURLs, function(i, u) {
-        $.ajax(u, {
-                type: 'GET',
-                dataType: 'jsonp',
-                success: function(data) {
-                    $.each(data, function(i, item) {
-                        displayTrolleyStops(item);
-                    });
-                }
-            });
-    });**/
-    event.preventDefault();
+        setInterval(getTrolleyLoc(route), 5000);
+        getTrolleyStops(route);
+        event.preventDefault();
+    }
   }); //End Trolley Button Event Handler
 
 $("#clear").on('click', function(event){
