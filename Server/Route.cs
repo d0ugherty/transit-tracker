62ddfe5d542830fa08c59773176d9333f1e43d45
sauce-picante <tdougherty16@gmail.com>
2022-10-18 11:32:56 -0400

    accessibility modifier updated

--- Server/Route.cs
+++ Server/Route.cs
@@ -1,13 +1,20 @@
-﻿namespace Server
+﻿using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Server
 {
     public class Route
     {
-        public int route_id { set; get; }
+        public int? route_id { set; get; }
         public string agency_id { set; get; }
         public string route_short_name { set; get; }
         public string route_long_name { set; get; }
-        public int route_type { set; get; }
+        public int? route_type { set; get; }
         public string route_url { set; get; }
         public string route_color { set; get; }
+
     }
 }
