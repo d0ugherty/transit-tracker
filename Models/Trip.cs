cb144abdba2dad8ae0e20e648a3f34d320533a66
sauce-picante <tdougherty16@gmail.com>
2022-10-20 08:38:53 -0400

    model for trips

---
+++ Models/Trip.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Transit_App.Models
+{
+    public class Trip
+    {
+        public int route_id { get; set; }
+        public int service_id { get; set; }
+        public int trip_id { get; set; }
+        public string trip_headsign { get; set; }
+        public int direction_id { get; set; }
+        public string block_id { get; set; }
+        public int shape_id { get; set; }
+    }
+}
