3d31f1abd172f8cb2c0f6eda9503c2f55dec9557
sauce-picante <tdougherty16@gmail.com>
2022-10-20 08:54:58 -0400

    added model for shapes

---
+++ Models/Shape.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Transit_App.Models
+{
+    public class Shape
+    {
+        public int shape_id { get; set; }
+        public double shape_pt_lat { get; set; }
+        public double shape_pt_lon { get; set; }
+        public  int shape_pt_sequence { get; set; }
+        public double shape_dist_traveled { get; set; }
+    }
+}
