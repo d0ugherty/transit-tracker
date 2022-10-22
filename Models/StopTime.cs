ab85538687769118bdd36e4306fdb2c95e121e1e
sauce-picante <tdougherty16@gmail.com>
2022-10-21 21:00:35 -0400

    added class for stop times

---
+++ Models/StopTime.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Transit_App.Models
+{
+    public class StopTime
+    {
+        public int trip_id { get; set; }
+        public string arrival_time { get; set; }
+        public string departure_time { get; set; }
+        public int stop_id { get; set; }
+        public int pickup_type { get; set; }
+        public int drop_off_type { get; set; }
+        public float shape_dist_travele { get; set; }
+
+    }
+}
